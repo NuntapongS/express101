01b0b00eff7404c2b5be30babdea654bcd8b4d53
Nuntapong <nuntapong@odds.team>
2022-10-11 16:00:40 +0700

    edit: call db initPrismaClient

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import { main } from "./app";
+import { DB } from "./config/db";
 
 const port = process.env.PORT ?? "8000";
 
 const start = () => {
+  DB.initPrismaClient();
   const app = main();
 
   app.listen(port, () => {
