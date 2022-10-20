d53139ed76ea69be52b231f246eada597aed6ecf
Nuntapong <nuntapong@odds.team>
2022-10-20 16:51:20 +0700

    edit: use middleware

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { userController } from "../controllers/user.controllers";
+import { verifyAPIKey } from "../middleware";
 
 export const userRoutes = express.Router();
 
 userRoutes.get("", userController.getUsers);
 userRoutes.get("/:id", userController.getUserByID);
-userRoutes.post("", userController.createUser);
+userRoutes.post("", verifyAPIKey(), userController.createUser);
 userRoutes.put("/:id", userController.updateUserByID);
 userRoutes.delete("/:id", userController.deleteUserByID);
